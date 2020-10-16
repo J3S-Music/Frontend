455f82be4e1e8c18c24ec0f783dd0e2d4b5a73b5
JujuEzay <63406890+JujuEzay@users.noreply.github.com>
2020-10-16 20:59:44 +0200

    Stand 16102020 no issues

--- src/app/session-beitreten/session-beitreten.component.ts
+++ src/app/session-beitreten/session-beitreten.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session-beitreten.component.scss']
 })
 export class SessionBeitretenComponent implements OnInit {
-
+            
   constructor() { }
 
   ngOnInit(): void {
