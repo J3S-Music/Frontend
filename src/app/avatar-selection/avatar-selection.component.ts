fd901af25d2fbb930b28032e00422118eb597fad
Simon <53291985+SiG268@users.noreply.github.com>
2020-10-27 01:46:45 +0100

    Fixed Avatar Selection

--- src/app/avatar-selection/avatar-selection.component.ts
+++ src/app/avatar-selection/avatar-selection.component.ts
@@ -1,34 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendcommService } from '../services/backendcomm.service';
 
+import { Avatar } from '../avatar-selection/avatar';
+import { AVATAR } from '../avatar-selection/list-data'
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-avatar-selection',
   templateUrl: './avatar-selection.component.html',
   styleUrls: ['./avatar-selection.component.scss']
-
+ 
 })
+
 export class AvatarSelectionComponent implements OnInit {
+  radioSel: any;
+  radioSelected: string;
+  radioSelectedString: string;
+  avatarList: Avatar[] = AVATAR;
 
-  content1: "";
-  content2: "";
-  content3: "";
-  content4: "";
-  content5: "";
-  content6: "";
 
-  constructor(private service: BackendcommService) { }
+  constructor(private service: BackendcommService, private router:Router) {
+    this.avatarList = AVATAR;
+    this.radioSelected = "Avatar_1";
+    this.getSelecteditem();
+  }
 
   ngOnInit(): void {
   }
+  onItemChange(avatar) {
+    this.getSelecteditem();
+  }
 
-  changeAvatar(){
-    /*this.service.changeAvatar(avatarID)
+  getSelecteditem() {
+    this.radioSel = AVATAR.find(Avatar => Avatar.value === this.radioSelected);
+    this.radioSelectedString = JSON.stringify(this.radioSel);
+  }
+
+  changeAvatar(id) {
+    console.log(id);
+    this.service.changeAvatar(id)
     .then(res=> {
     // Success
     console.log(res);
+      this.router.navigate(["/settings"]);
     })
     .catch(error =>{
       console.log(error)                                        //error werfen
-    })*/
+    })
   }
 }
---
+++ src/app/avatar-selection/avatar.ts
@@ -0,0 +1,4 @@
+export class Avatar{
+    name:string;
+    value:string;
+}
