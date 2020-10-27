f0e796f38a87181313078cc377bd6d87c0e49878
Simon <53291985+SiG268@users.noreply.github.com>
2020-10-27 17:32:52 +0100

    display default connection in settings

--- src/app/einstellungen/einstellungen.component.ts
+++ src/app/einstellungen/einstellungen.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import{BackendcommService} from '../services/backendcomm.service';  //service importieren
-
-
-import { Avatar } from '../avatar-selection/avatar';
-import { AVATAR } from '../avatar-selection/list-data'
 import { Router } from '@angular/router';
 
 @Component({
@@ -20,8 +16,7 @@ export class EinstellungenComponent implements OnInit {
 public userid = '';
 public name = '';
 public avatar = '';
-
-
+public _default='';
 
 //*****************************************GET BEFEHL*************************************************/
 ngOnInit(): void {
@@ -32,17 +27,29 @@ ngOnInit(): void {
     this.name = res['name'];
     this.userid = res['userID'];
     this.avatar = '../assets/' + res['avatar']['pictureName'];
-    //this.avatar = '../assets/' + res['user']['avatar'];
-    //console.log(res['products']);
+
+    var connectionList = res['userConnections'];
+    this._default=this.getDefault(connectionList);
     })
-    
     .catch(error =>{
       console.log(error +' Keine userdaten')                  //error werfen
     })                                                        //resolve-> gehts in then bei catch also fehler reject
+    
 
 
 }
-
+    
+  getDefault(list):string{
+    var def = '';
+    list.forEach(function(value){
+      if(value['_default']){
+        def = value['connection']['name'] as string
+        console.log("Found Default: "+def);
+        return def;
+      }
+    });
+    return def;
+  }
 
   test(){
     this.service.getData()
@@ -57,4 +64,24 @@ ngOnInit(): void {
       })                                                          //resolve-> gehts in then bei catch also fehler reject
 
   }
+
+
+  changeDefault(connection){
+    var _default='';
+
+    
+    
+  }
+  setchecked(item){
+    console.log(item);
+
+
+  }
+  isDefault(connection){
+    //console.log(this._default);
+    //console.log(this._default===connection);
+    return this._default===connection;
+  }
+
+
 }
