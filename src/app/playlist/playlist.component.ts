ab0526a9a530db10885397b439c6717eab6276e1
PhilippEck <34600034+PhilippEck@users.noreply.github.com>
2020-10-31 00:59:05 +0100

    enter taste zum bestätigen und tabellen header

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -9,37 +9,38 @@ import { Song } from './Song';
   styleUrls: ['./playlist.component.scss']
 })
 export class PlaylistComponent implements OnInit {
-  public songs:Song[] = new Array();
+  public songs: Song[] = new Array();
   constructor(private service: BackendcommService) { }
 
-
+  headElements = ['Cover', 'Track', 'Artist', 'Album'];
 
   FormSearch = new FormControl('');
   ngOnInit(): void {
   }
 
 
-  search(){
+  search() {
     //clear songs Array
-    this.songs=[];
+    this.songs = [];
     //Get Search Query from User Input
-    const query = this.FormSearch.value; 
+    const query = this.FormSearch.value;
     //Call Backend
     this.service.search(query)
-    .then(res => {
-      //Save found songs in songs Array
-      res['items'].forEach(Object => {
-        var song =new Song().deserialize(Object);
-        this.songs.push(song);
+      .then(res => {
+        //Save found songs in songs Array
+        res['items'].forEach(Object => {
+          var song = new Song().deserialize(Object);
+          this.songs.push(song);
+        });
+        console.log(this.songs);
+      })
+      .catch(error => {
+        console.log(error);                  // error werfen
       });
-      console.log(this.songs);
-    })
-    .catch(error => {
-      console.log(error);                  // error werfen
-    });
-
   }
 
-
+  checkStatus(): boolean {
+    return this.songs.length !== 0;
+  }
 
 }
